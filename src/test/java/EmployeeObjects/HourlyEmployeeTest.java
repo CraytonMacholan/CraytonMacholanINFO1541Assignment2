8176a8125171b85e84a56588f7f77849735aed30
CraytonMacholan <24macholancrayton@blairschools.org>
2024-03-27 10:37:07 -0500

    IncreaseHoursIsPositive, AnnualRaise, CalculateWeeklyPay_NormalHours, CalculateWeeklyPay_OT

--- src/test/java/EmployeeObjects/HourlyEmployeeTest.java
+++ src/test/java/EmployeeObjects/HourlyEmployeeTest.java
@@ -1,6 +1,5 @@
 package EmployeeObjects;
 
-
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,5 +12,25 @@ class HourlyEmployeeTest {
         emp.increaseHours(10);
         emp.increaseHours(-5);
         assertEquals(10, emp.getHoursWorked());
+    }
+    @Test
+    void annualRaise() {
+        HourlyEmployee emp = new HourlyEmployee("Tony", "Stark", 5749, "Service", "Lead Service Manager", 32.85);
+        emp.annualRaise();
+        assertEquals(34.49, emp.getWage(), 0.01);
+    }
+
+    @Test
+    void calculateWeeklyPay_NormalHours() {
+        HourlyEmployee emp = new HourlyEmployee("Tony", "Stark", 5749, "Service", "Lead Service Manager", 32.85);
+        emp.increaseHours(35); // Sending 35 hours
+        assertEquals(1149.75, emp.calculateWeeklyPay(), 0.01);
+    }
+
+    @Test
+    void calculateWeeklyPay_OT() {
+        HourlyEmployee emp = new HourlyEmployee("Tony", "Stark", 5749, "Service", "Lead Service Manager", 32.85);
+        emp.increaseHours(45); // Sending 45 hours
+        assertEquals(1560.38, emp.calculateWeeklyPay(), 0.01);
     }
 }
